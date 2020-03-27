3464da4a5dc920a92c798bbc1607f6e793376db1
Michel Bernardo <bernardoenglish@outlook.com>
2020-03-27 14:01:09 -0400

    finalização do projeto com testes

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -8,13 +9,51 @@ const routes = express.Router();
 
 routes.post('/sessions', SessionController.create);
 
-routes.get('/ongs', OngController.index); 
-routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
 
-routes.get('/profile', ProfileController.index);
+/** Validação de dados na criação da ONG */
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(9).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+/** Validação de dados na exibição do perfil de uma ONG */
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+/** Validação de exibição dos CASOS de uma ONG */
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.index);
+
+/** Validação de dados na criação de CASOS */
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required().min(10).max(25),
+        description: Joi.string().required().min(20).max(50),
+        value: Joi.number().required(),
+        ong_id: Joi.required(),
+    })
+}), IncidentController.create);
+
+/** Validação de ID para deletar os CASOS */
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 module.exports = routes;
